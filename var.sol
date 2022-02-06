62b56a7ebfcfc118b46f3d5df4131b1ac2faab2b
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-02-06 22:06:37 +0530

    Create var.sol

---
+++ var.sol
@@ -0,0 +1,11 @@
+pragma solidity  ^0.6.0;
+
+contract myContract{
+// uint mk = 5;
+struct Mystruct{
+    uint myUint;
+    string myString;
+}
+
+Mystruct public mk = Mystruct(21,"Krushna Modhave");
+}
