8565efda71b180e0c2b606254fff79c132aae0e5
krishmk1009 <76839752+krishmk1009@users.noreply.github.com>
2022-02-25 16:28:48 +0530

    Create Practice.sol
    practice code about state variable , local var , constructor , function - get & set , pure vs view

---
+++ Practice.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.0;
+
+contract myContract{
+
+    // uint public age;
+    // age=10;
+
+// function info() pure public returns(uint){
+//     string memory name = "mk";
+//     uint age = 15 ;
+//     return age;
+// }
+
+// uint public age = 20;
+
+// // function get() public view returns(uint){
+// //     return age;
+// // }
+// function set(uint sum) public {
+//     age = sum+age;
+// }
+
+// uint age = 10;
+
+// function get() pure public returns(uint){
+//    uint name = 15;
+//     return name;
+// }
+
+uint public count ;
+
+constructor(uint value){
+    count  = value;
+}
+ }
